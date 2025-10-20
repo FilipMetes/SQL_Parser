b1698fc8d77b748a3f7c849139d7d871248dff28
FIlip Metes <filipmetes04@gmail.com>
2025-10-20 17:38:09 +0200

    GUI-DEMo for Wallet and nonWallet connection (working)

--- src/database/ConnectionWindow.java
+++ src/database/ConnectionWindow.java
@@ -1,38 +1,58 @@
 package database;
 
 import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class ConnectionWindow extends Application {
+    private final VBox window;
+    private final HBox header;
     private final VBox root;
 
     public ConnectionWindow() {
+        this.window = new VBox();
         this.root = new VBox(10);
+        this.root.setFillWidth(false);
+        root.setPadding(new Insets(0, 0, 0, 20));
+        this.header = new HBox();
+
+        this.window.getChildren().addAll(this.header, this.root);
     }
     @Override
     public void start(Stage primaryStage) {
 
-        Button walletConnection = new Button("Connect without wallet");
-        Button nonWalletConnection = new Button("Connect with wallet");
+        Button walletConnection = new Button("Connect with wallet");
+        Button nonWalletConnection = new Button("Connect without wallet");
+        Button returnButton = new Button("Return");
 
-        this.root.getChildren().add(nonWalletConnection);
-        this.root.getChildren().add(walletConnection);
+        this.header.getChildren().addAll(nonWalletConnection, walletConnection, returnButton);
 
-        walletConnection.setOnAction(e -> {
-            this.root.getChildren().remove(walletConnection);
-            this.initializeNonWalletConnection();
+        this.header.setAlignment(Pos.CENTER);
+
+
+        returnButton.setOnAction(e -> {
+            this.clearRoot();
         });
 
-        nonWalletConnection.setOnAction(e -> {
-            this.root.getChildren().remove(nonWalletConnection);
+        walletConnection.setOnAction(e -> {
+            this.clearRoot();
             this.initializeWalletConnection();
         });
 
-        Scene scene = new Scene(root, 400, 300);
+        nonWalletConnection.setOnAction(e -> {
+            this.clearRoot();
+            this.initializeNonWalletConnection();
+        });
+
+
+        Scene scene = new Scene(this.window, 400, 300);
+
         primaryStage.setTitle("SQL-Parser_DEMO");
         primaryStage.setScene(scene);
         primaryStage.show();
@@ -48,10 +68,15 @@ public class ConnectionWindow extends Application {
 
     public void initializeNonWalletConnection() {
         TextArea host = this.createTextArea("Host");
+        host.setPrefWidth(150);
         TextArea port = this.createTextArea("Port");
+        port.setPrefWidth(150);
         TextArea sid = this.createTextArea("Sid");
+        sid.setPrefWidth(150);
         TextArea user = this.createTextArea("User");
+        user.setPrefWidth(150);
         TextArea password = this.createTextArea("Password");
+        password.setPrefWidth(150);
 
         Button submitButton = new Button("Connect");
 
@@ -63,7 +88,7 @@ public class ConnectionWindow extends Application {
             String userSid = sid.getText();
             String userUser = user.getText();
             String userPassword = password.getText();
-            Connect connect = new Connect(userHost, userPort, userSid, userUser, userPassword);
+            Connect connect = new Connect( userHost, userPort, userSid, userUser, userPassword);
             root.getChildren().removeAll(host, port, sid, user, password, submitButton);
         });
 
@@ -71,9 +96,13 @@ public class ConnectionWindow extends Application {
 
     public void initializeWalletConnection() {
         TextArea user = this.createTextArea("User");
+        user.setPrefWidth(150);
         TextArea password = this.createTextArea("Password");
+        password.setPrefWidth(150);
         TextArea walletPath = this.createTextArea("WalletPath");
+        walletPath.setPrefWidth(150);
         TextArea tsnAlias = this.createTextArea("tsn_Alias");
+        tsnAlias.setPrefWidth(150);
 
         Button submitButton = new Button("Connect");
         root.getChildren().addAll(user, password, walletPath, tsnAlias, submitButton);
@@ -87,6 +116,12 @@ public class ConnectionWindow extends Application {
             root.getChildren().removeAll(user, password, walletPath, tsnAlias, submitButton);
         });
 
+    }
+
+    public void clearRoot() {
+        if (!this.root.getChildren().isEmpty()) {
+            this.root.getChildren().clear();
+        }
     }
 }
 
